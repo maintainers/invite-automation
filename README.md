777f574e21b2588380300195a818a0e7ce7a1622
Rizel Scarlett <blackgirlbytes@github.com>
2021-11-28 17:52:09 +0000

    moved to probot app

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # invite-automation
-This is a repo to automate invites to teams. 
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that automate invites to teams via interacting with a repo
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Docker
+
+```sh
+# 1. Build container
+docker build -t invite-automation .
+
+# 2. Start container
+docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> invite-automation
+```
+
+## Contributing
+
+If you have suggestions for how invite-automation could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2021 GitHub <undefined>
---
+++ index.js
@@ -0,0 +1,119 @@
+const { Octokit } = require("octokit");
+const octokit = new Octokit({
+  auth: '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',
+})
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ * 
+ * 
+ */
+
+async function checkForEmployee(user) {
+  let employeeStatus
+  const result = await octokit.request('GET /orgs/{org}/teams/{team_slug}/memberships/{username}', {
+    org: 'github',
+    team_slug: 'employees',
+    username: user
+  }).catch(err => {
+    if (err.status === 404) {
+      console.log('user is not an employee')
+      employeeStatus = false
+    }
+  });
+  if (result) {
+    if (result.status === 200) {
+      console.log("user is employee of github", result.status);
+      return true;
+    }
+  }
+  return employeeStatus;
+}
+
+async function addUserToTeam(user, employeeStatus) {
+  const team_slug = employeeStatus ? 'github-employees' : 'members';
+  console.log(team_slug, 'team slug');
+  const result = await octokit.request('PUT /orgs/{org}/teams/{team_slug}/memberships/{username}', {
+    org: 'maintainers',
+    team_slug: team_slug,
+    username: user,
+    role: 'member'
+  }).catch(err => {
+    if (err.status === 403) {
+      console.log(`Forbidden ${err.status}`)
+    }
+    if (err.status === 422) {
+      console.log(`Unprocessable Entity ${err.status}`);
+    }
+  }
+  );
+  if (result) {
+    if (result.status === 200) {
+      console.log("user added to team", result.status);
+    }
+  }
+}
+
+async function openIssue(user) {
+  const result = await octokit.request('POST /repos/{owner}/{repo}/issues', {
+    owner: 'maintainers',
+    repo: 'invite-automation',
+    labels: ['pending-invitation'],
+    title: `Pending invitation request for: @${user}`,
+    body: `Please take actions on adding the requested user to this repo:
+    
+- [ ] Assign this ticket to the correct user
+- [ ] Confirm that @${user} should be added as a member
+- [ ] Send an invite
+*This issue body is just a placeholder and will change as this project grows*`,
+  }).catch(err => {
+    if (err.status === 403) {
+      console.log(`Forbidden ${err.status}`)
+    }
+    if (err.status === 422) {
+      console.log(`Unprocessable Entity ${err.status}`);
+    }
+  }
+  );
+
+  if (result === 201) {
+    console.log("opened an issue", result);
+  }
+}
+
+async function assignTeams(user) {
+  const result = await octokit.request("GET /orgs/{org}/members/{username}", {
+    username: user,
+    org: "maintainers",
+  }).catch(err => {
+    if (err.status === 404) {
+      console.log("user is not a team member but part of the org", err.status);
+      openIssue(user);
+    }
+  });
+  
+  if (result) {
+    if (result.status === 204) {
+      console.log("user is part of org", result);
+      const employeeStatus = await checkForEmployee(user);
+      console.log(employeeStatus, 'employee status');
+      addUserToTeam(user, result);
+
+    }
+    if (result.status === 302) {
+      console.log("user is not part of org", result);
+      openIssue(user);
+    }
+  }
+}
+
+module.exports = (app) => {
+  app.on("star", async (context) => {
+    app.log.info(`the user is ${context.payload.sender.login}`)
+    const user = context.payload.sender.login
+    app.log.info('opening an issue')
+    if (user) {
+      await assignTeams(user)
+    }
+  });
+};
